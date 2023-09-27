c6dc2063ff96dffd2e93af3d4703b21208ca009a
AdryanAlura <109964914+AdryanAlura@users.noreply.github.com>
2023-09-27 11:27:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ function TocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-
-
 const listaDeteclas = document.querySelectorAll('.tecla');
 
+listaDeteclas[0].onclick = TocaSomPom;
 
+//enquanto
+while () {
+    
+}
